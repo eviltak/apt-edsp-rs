9c97ff4b1366db812fc9b1154c6cc09789543be2
Arav Singhal <singhal.arav@gmail.com>
2024-07-09 14:05:55 -0500

    scenario: Add package relationships model

--- src/scenario.rs
+++ src/scenario.rs
@@ -1,7 +1,11 @@
 use std::collections::HashMap;
+use std::fmt::Display;
 use std::io::BufRead;
+use std::str::FromStr;
 
 use serde::{Deserialize, Serialize};
+
+use super::Version;
 
 pub struct Scenario {
     pub request: Request,
@@ -80,6 +84,132 @@ pub struct Package {
     pub extra: HashMap<String, String>,
 }
 
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum Relation {
+    Earlier,
+    EarlierEqual,
+    Equal,
+    LaterEqual,
+    Later,
+}
+
+impl Relation {
+    pub fn parse<'a, E: nom::error::ParseError<&'a str>>(input: &'a str) -> nom::IResult<&str, Self, E> {
+        use nom::branch::alt;
+        use nom::bytes::complete::tag;
+        use nom::combinator::value;
+        alt((
+            value(Relation::Earlier, tag("<<")),
+            value(Relation::EarlierEqual, tag("<=")),
+            value(Relation::Equal, tag("=")),
+            value(Relation::LaterEqual, tag(">=")),
+            value(Relation::Later, tag(">>")),
+        ))(input)
+    }
+}
+
+impl Display for Relation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Relation::Earlier => write!(f, "<<"),
+            Relation::EarlierEqual => write!(f, "<="),
+            Relation::Equal => write!(f, "="),
+            Relation::LaterEqual => write!(f, ">="),
+            Relation::Later => write!(f, ">>"),
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Relationship {
+    pub package: String,
+    pub constraint: Option<(Relation, Version)>,
+}
+
+impl Display for Relationship {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.package)?;
+
+        if let Some((relation, version)) = &self.constraint {
+            write!(f, " ({} {})", relation, version.as_str())?;
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub enum RelationshipParseError {
+    EmptyPackageName(String),
+    BadConstraintSpec(String),
+    BadVersion(<Version as TryFrom<&'static str>>::Error),
+}
+
+impl Display for RelationshipParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RelationshipParseError::EmptyPackageName(e) => write!(f, "Error parsing package name: {}", e),
+            RelationshipParseError::BadConstraintSpec(e) => write!(f, "Error parsing constraint spec: {}", e),
+            RelationshipParseError::BadVersion(e) => write!(f, "Error parsing version: {}", e)
+        }
+    }
+}
+
+impl FromStr for Relationship {
+    type Err = RelationshipParseError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        use nom::Finish;
+        use nom::bytes::complete::*;
+        use nom::character::complete::*;
+        use nom::combinator::*;
+        use nom::sequence::*;
+
+        let (remaining, package) = terminated(
+            take_while1(|c: char| !c.is_whitespace() && c != '('),
+            space0,
+        )(input)
+            .finish()
+            .map_err(|e| RelationshipParseError::EmptyPackageName(nom::error::convert_error(input, e)))?;
+
+        // Parse constraint
+        let constraint = if remaining.is_empty() {
+            None
+        } else {
+            let (_, (relation, version)) = all_consuming(preceded(
+                char('('),
+                terminated(
+                    separated_pair(Relation::parse, space0, take_until1(")")),
+                    tuple((char(')'), space0)),
+                ),
+            ))(remaining)
+                .finish()
+                .map_err(|e| RelationshipParseError::BadConstraintSpec(nom::error::convert_error(input, e)))?;
+            let version = Version::try_from(version).map_err(RelationshipParseError::BadVersion)?;
+            Some((relation, version))
+        };
+
+        Ok(Self {
+            package: package.to_string(),
+            constraint,
+        })
+    }
+}
+
+impl Serialize for Relationship {
+    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        serializer.collect_str(self)
+    }
+}
+
+impl<'de> Deserialize<'de> for Relationship {
+    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        String::deserialize(deserializer)
+            .and_then(|v| v.parse().map_err(serde::de::Error::custom))
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use indoc::indoc;
@@ -91,33 +221,92 @@ mod tests {
         val: T,
     }
 
-    impl<T: Eq + Serialize + Deserialize<'static> + std::fmt::Debug> TestCase<T> {
-        fn check(&self) {
-            assert_eq!(self.repr, rfc822_like::to_string(&self.val).unwrap());
-            assert_eq!(self.val, rfc822_like::from_str(self.repr).unwrap());
+    impl<T: Eq + Serialize + Deserialize<'static> + std::fmt::Debug> TestCase<T>
+    {
+        fn check<FS, FD>(&self, serialize_fn: FS, deserialize_fn: FD)
+        where
+            FS: Fn(&T) -> String,
+            FD: Fn(&'static str) -> T,
+        {
+            assert_eq!(self.val, (deserialize_fn)(self.repr), "Incorrect deserialized value from '{}' (left: expected, right: actual)", self.repr);
+            assert_eq!(self.repr, (serialize_fn)(&self.val), "Incorrect serialized value from '{:?}' (left: expected, right: actual)", self.val);
         }
     }
 
-    macro_rules! tests {
-        ($($name:ident: {$repr:expr, $val:expr})*) => {
-            $(
-                #[test]
-                fn $name() {
+    fn struct_to_string<T: Serialize>(val: &T) -> String {
+        match rfc822_like::to_string(val) {
+            Ok(t) => t,
+            Err(e) => panic!("Error when serializing: {e}"),
+        }
+    }
+
+    fn struct_from_str<'de, T: Deserialize<'de>>(s: &'de str) -> T {
+        match rfc822_like::from_str(s) {
+            Ok(t) => t,
+            Err(e) => panic!("Error when deserializing: {e}"),
+        }
+    }
+
+    fn value_to_string<T: Serialize>(val: &T) -> String {
+        #[derive(Serialize)]
+        struct Record<'a, V> {
+            val: &'a V,
+        }
+
+        struct_to_string(&Record { val }).trim()["val: ".len()..].to_string()
+    }
+
+    fn value_from_str<T: for<'de> Deserialize<'de>>(s: &str) -> T {
+        #[derive(Deserialize)]
+        struct Record<V> {
+            val: V,
+        }
+
+        struct_from_str::<Record<T>>(&format!("val: {s}")).val
+    }
+
+    macro_rules! serde_test {
+        ($name:ident: {$($repr:expr => $val:expr),+}) => {
+            serde_test!(
+                $name(
+                struct_to_string,
+                struct_from_str
+                ): {$($repr => $val),+}
+            );
+        };
+
+        ($name:ident($serialize_fn:expr, $deserialize_fn:expr): {$($repr:expr => $val:expr),+}) => {
+            serde_test!(@test
+                $name,
+                $serialize_fn,
+                $deserialize_fn, $($repr, $val)+;
+            );
+        };
+
+        (@test $name:ident, $serialize_fn:expr, $deserialize_fn:expr, $($repr:expr, $val:expr)+;) => {
+            #[test]
+            fn $name() {
+                $(
+                {
                     let repr = { $repr };
                     let val = { $val };
-                    TestCase { repr, val, }.check();
+                    TestCase {
+                        repr,
+                        val,
+                    }.check($serialize_fn, $deserialize_fn);
                 }
-            )*
+                )+
+            }
         };
     }
 
-    tests! {
+    serde_test! {
         request: {
             indoc! {"
                 Request: EDSP 0.5
                 Architecture: amd64
                 Upgrade-All: yes
-            "},
+            "} =>
             Request {
                 request: "EDSP 0.5".into(),
                 architecture: "amd64".into(),
@@ -128,6 +317,9 @@ mod tests {
                 ..Default::default()
             }
         }
+    }
+
+    serde_test! {
         vec_request: {
             indoc! {"
                 Request: EDSP 0.5
@@ -137,7 +329,7 @@ mod tests {
                 Request: EDSP 0.5
                 Architecture: amd64
                 Upgrade-All: no
-            "},
+            "} =>
             vec![
                 Request {
                     request: "EDSP 0.5".into(),
@@ -156,6 +348,65 @@ mod tests {
                         ..Default::default()
                     },
                     ..Default::default()
+                }
+            ]
+        }
+    }
+
+    serde_test! {
+        relationship(value_to_string, value_from_str): {
+            "foo" =>
+            Relationship {
+                package: "foo".into(),
+                constraint: None,
+            },
+            "foo (<< 2.2.1)" =>
+            Relationship {
+                package: "foo".into(),
+                constraint: Some((Relation::Earlier, Version::try_from("2.2.1").unwrap())),
+            },
+            "foo (<= 2.2.1)" =>
+            Relationship {
+                package: "foo".into(),
+                constraint: Some((Relation::EarlierEqual, Version::try_from("2.2.1").unwrap())),
+            },
+            "foo (= 2.2.1)" =>
+            Relationship {
+                package: "foo".into(),
+                constraint: Some((Relation::Equal, Version::try_from("2.2.1").unwrap())),
+            },
+            "foo (>= 2.2.1)" =>
+            Relationship {
+                package: "foo".into(),
+                constraint: Some((Relation::LaterEqual, Version::try_from("2.2.1").unwrap())),
+            },
+            "foo (>> 2.2.1)" =>
+            Relationship {
+                package: "foo".into(),
+                constraint: Some((Relation::Later, Version::try_from("2.2.1").unwrap())),
+            }
+        }
+    }
+
+    serde_test! {
+        vec_relationship(value_to_string, value_from_str): {
+            indoc! {"
+                foo,
+                     bar,
+                     baz
+            "}.trim() =>
+            vec![
+                Relationship {
+                    package: "foo".into(),
+                    constraint: None,
+                },
+                Relationship {
+                    package: "bar".into(),
+                    constraint: None,
+                },
+                Relationship {
+                    package: "baz".into(),
+                    constraint: None,
                 }
             ]
         }
