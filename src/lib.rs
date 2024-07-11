1d653c8dafec25a0f55381a532f6e342eaec0343
Arav Singhal <singhal.arav@gmail.com>
2024-07-11 15:20:53 -0500

    doc: Document top-level items

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,20 @@
+//! A Rust data model of the [APT External Dependency Solver Protocol][apt-edsp].
+//! Useful for writing custom dependency solvers for [APT] in Rust.
+//!
+//! [apt-edsp]: https://salsa.debian.org/apt-team/apt/-/blob/a8367745eac915281cc2b9fb98813e9225d1e55c/doc/external-dependency-solver-protocol.md
+//! [APT]: https://en.wikipedia.org/wiki/APT_(software)
+
 pub use bool::Bool;
 pub use progress::Progress;
 
+/// Contains the models for [EDSP answers].
+///
+/// [EDSP answers]: https://salsa.debian.org/apt-team/apt/-/blob/a8367745eac915281cc2b9fb98813e9225d1e55c/doc/external-dependency-solver-protocol.md#answer
 pub mod answer;
+
+/// Contains the models for the EDSP input (a [scenario]).
+///
+/// [scenario]: https://salsa.debian.org/apt-team/apt/-/blob/a8367745eac915281cc2b9fb98813e9225d1e55c/doc/external-dependency-solver-protocol.md#scenario
 pub mod scenario;
 
 mod bool;
--- src/progress.rs
+++ src/progress.rs
@@ -1,5 +1,8 @@
 use serde::{Deserialize, Serialize};
 
+/// The model describing a [`Progress` stanza].
+///
+/// [`Progress` stanza]: https://salsa.debian.org/apt-team/apt/-/blob/a8367745eac915281cc2b9fb98813e9225d1e55c/doc/external-dependency-solver-protocol.md#progress
 #[derive(Serialize, Deserialize, Debug, Default, Eq, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct Progress {
