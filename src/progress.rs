328747d287804c07ce3c921beb2a55f190c69303
Arav Singhal <singhal.arav@gmail.com>
2024-08-05 10:35:21 -0500

    feat: Add `Progress::write_to` method

--- src/progress.rs
+++ src/progress.rs
@@ -10,3 +10,13 @@ pub struct Progress {
     pub percentage: Option<String>,
     pub message: Option<String>,
 }
+
+impl Progress {
+    pub fn write_to(&self, writer: impl std::io::Write) -> Result<(), ProgressWriteError> {
+        rfc822_like::to_writer(writer, self).map_err(Into::into)
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+#[error(transparent)]
+pub struct ProgressWriteError(#[from] rfc822_like::ser::Error);
